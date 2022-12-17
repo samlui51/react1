27d0575e0c9bfb5e79faa4f655e62888ecf6b6c9
samlui51 <64849308+samlui51@users.noreply.github.com>
2022-12-17 13:53:48 +0800

    little fix

--- src/components/Pages/TVShow.js
+++ src/components/Pages/TVShow.js
@@ -7,10 +7,12 @@ const TVShow = () => {
     <React.Fragment>
       <Header></Header>
       <div className="background">
-        <div className="description">
-          <div className="title">節目</div>
-          <div className="content">
-            這年頭小螢幕作品也能大放異彩。從情境劇、戲劇、旅遊節目到脫口秀，最精彩的電視節目都在這裡。
+        <div className="top">
+          <div className="description">
+            <div className="title">節目</div>
+            <div className="content">
+              這年頭小螢幕作品也能大放異彩。從情境劇、戲劇、旅遊節目到脫口秀，最精彩的電視節目都在這裡。
+            </div>
           </div>
         </div>
       </div>
